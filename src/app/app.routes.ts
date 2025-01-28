94b2794e5bad2d35dfb242097ecceb3828851191
Habeeb-Rahman-CA <habeebrahmanca07@gmail.com>
2025-01-28 10:50:23 +0530

    project setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutMeComponent } from './components/pages/about-me/about-me.component';
+import { ContactComponent } from './components/pages/contact/contact.component';
+import { WorksComponent } from './components/pages/works/works.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '**', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'about-me', component: AboutMeComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'works', component: WorksComponent }
+];
