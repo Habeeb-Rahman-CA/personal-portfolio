e7543ed4073632b3c71a5c5e05daa62ac2e23e05
HabeebR07 <habeebrahmanca07@gmail.com>
2025-02-01 00:24:29 +0530

    added new about section and updated home and nav

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
+import { ScrollService } from '../../../services/scroll.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  scrollService = inject(ScrollService)
 
+  @HostListener('window:scroll', [])
+  onWindowScroll(){
+    this.scrollService.updateScrollY(window.scrollY)
+  }
 }
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { ScrollService } from '../../../services/scroll.service';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  hideNavbar:boolean = false
 
+  scrollService = inject(ScrollService)
+
+  ngOnInit(): void {
+      this.scrollService.scrollY$.subscribe(scrollY =>{
+        this.hideNavbar = scrollY > 140
+      })
+  }
 }
