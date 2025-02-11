735c5c00970396c2932213b301f6e1e701180d7d
habeeburahman07 <habeebu@seeroo.com>
2025-02-11 13:13:00 +0530

    changing the dummy data into the dynamic for structuring the code

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatIconModule } from '@angular/material/icon'
 import { CommonModule } from '@angular/common';
+import { IWork } from '../../../interface/works.interface';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,30 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent {
   scrollService = inject(ScrollService);
+
+  works: IWork[] = [
+    {
+      name: "ArtOgram",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa voluptas beatae qui inventoreexplicabo exdolor deleniti molestias, corrupti pariatur eius omnis nemo architecto laudantium quis reiciendis voluptatibus rem. Vitae? lorem",
+      imgUrl: "artOgram.png",
+      link: "#",
+      tools: ["MongoDB", "Express.js", "Angular", "Node.js", "Razorpay"]
+    },
+    {
+      name: "Loom Collective",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa voluptas beatae qui inventoreexplicabo exdolor deleniti molestias, corrupti pariatur eius omnis nemo architecto laudantium quis reiciendis voluptatibus rem. Vitae? lorem",
+      imgUrl: "loomCollective.png",
+      link: "http://loomcollective.netlify.app",
+      tools: ["PostgreSQL", "Nest.js", "Angular", "Node.js"]
+    },
+    {
+      name: "Bitora.",
+      desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Culpa voluptas beatae qui inventoreexplicabo exdolor deleniti molestias, corrupti pariatur eius omnis nemo architecto laudantium quis reiciendis voluptatibus rem. Vitae? lorem",
+      imgUrl: "birora.png",
+      link: "http://bitora.netlify.app",
+      tools: ["HTML", "CSS", "Javascript", "Chart.js", "TradingView"]
+    },
+  ]
 
   @HostListener('window:scroll', [])
   onWindowScroll() {
---
+++ src/app/interface/works.interface.ts
@@ -0,0 +1,7 @@
+export interface IWork {
+    name: string;
+    desc: string;
+    imgUrl: string;
+    link: string;
+    tools: string[];
+}
